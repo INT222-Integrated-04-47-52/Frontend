3c335cae8ca5f144608d231cc6f62a1e8a583c90
Kpinnoes <khorapin.pingadpu@mail.kmutt.ac.th>
2021-08-21 21:48:21 +0700

    [Updated] Contact

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,37 +1,56 @@
 import '../components/cssComponent/decorate.css';
 import React from 'react';
-
+import Pin from '../../img/PinNookNooch/Pin.jpg'
 function Contacts(){
   return( 
-  
-    <section className="contact contactPosition">
-        <div  className="container" >
-            {/* <div className="row"> */}
-                <div className="col-lg-6 col-md-6">
-                    <div className="contact__text">
-                        <div className="section-title">
-                            <h2>Contact Us</h2>
-                            <p>KMUTT | School of Information Technology</p>
-                        </div>
-                        <ul>
-                            <li>
-                                <h4>62130500004</h4>
-                                <p>Khorapin Gadpu <br />Khorapin.pingadpu@mail.kmutt.ac.th</p>
-                            </li>
-                            <li>
-                                <h4>62130500047</h4>
-                                <p>Noochajee Phonbooncharoenchai <br />Noochajee.nn@mail.kmutt.ac.th</p>
-                            </li>
-                            <li>
-                                <h4>62130500052</h4>
-                                <p>Prapaporn Sira <br />Prapaporn.1412@mail.kmutt.ac.th</p>
-                            </li>
-                        </ul>
-                    </div>
+<section ClassName="contact spad">
+<div ClassName="container">
+    <div ClassName="row">
+        <div ClassName="col-lg-6 col-md-6">
+            <div ClassName="contact__text">
+                <div ClassName="section-title">
+                    <span>Information</span>
+                    <h2>Contact Us</h2>
+                    <p>KMUTT | School of Information Technology</p>
                 </div>
-            {/* </div> */}
+                <ul>
+                    <li>
+                        <img src={Pin}/>
+                    <h4>62130500004</h4>
+                                <p>Khorapin Gadpu <br />Khorapin.pingadpu@mail.kmutt.ac.th</p>
+                          </li>
+                    <li>
+                    <h4>62130500047</h4>
+                                <p>Noochajee Phonbooncharoenchai <br />Noochajee.nn@mail.kmutt.ac.th</p>
+                             </li>
+                             <li>
+                                <h4>62130500052</h4>
+                                <p>Prapaporn Sila <br />Prapaporn.1412@mail.kmutt.ac.th</p>
+                            </li>
+                </ul>
+            </div>
         </div>
-    </section>
+        <div ClassName="col-lg-6 col-md-6">
+            <div ClassName="contact__form">
+                <form action="#">
+                    <div ClassName="row">
+                        <div ClassName="col-lg-6">
+                            <input type="text" placeholder="Name"/>
+                        </div>
+                        <div ClassName="col-lg-6">
+                            <input type="text" placeholder="Email"/>
+                        </div>
+                        <div ClassName="col-lg-12">
+                            <textarea placeholder="Message"></textarea>
+                            <button type="submit" ClassName="site-btn">Send Message</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+</section>
 
     );
   };
--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -12,7 +12,6 @@ function Footer(){
     <div class="row">
         <div class="col-lg-12 text-center">
             <div class="footer__copyright__text">
-              
                 <p>Copyright ©
                      2021 INT222 Integrated Project. Proudly by Khorapin, Noochajee and Prapaporn
                 </p>
