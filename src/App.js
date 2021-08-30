dbe4fc6e3a45a40979f76809a75cc89e68fa4631
Kpinnoes <khorapin.pingadpu@mail.kmutt.ac.th>
2021-08-30 21:55:22 +0700

    [Update] App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route,Switch, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import Shop  from './pages/Shop';
 import Contacts from './pages/Contacts';
 import Footer from './pages/Footer';
-
+import React, { Component } from "react";
 import Navbar from './pages/NavBar';
 import './App.css';
-import { Switch } from 'react-router-dom/cjs/react-router-dom.min';
 import SignIn from './pages/Signin';
 import Tailor from './pages/ShopComponent/Tailor';
 import AddProduct from './pages/Admin/AddProduct';
 import AddSize from './pages/ShopComponent/AddSize';
-function App() {
-  return (
-    <Router>
+import Cart from './pages/ShopComponent/Cart';
+import Login from './pages/ShopComponent/Login';
+import ProductList from './pages/ShopComponent/ProductList';
+import Context from "./Context";
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+      cart: {},
+      products: []
+    };
+    this.routerRef = React.createRef();
+  }
+  render() {
+    return (
+    <Context.Provider
+    value={{
+      ...this.state,
+      removeFromCart: this.removeFromCart,
+      addToCart: this.addToCart,
+      login: this.login,
+      addProduct: this.addProduct,
+      clearCart: this.clearCart,
+      checkout: this.checkout
+    }}
+  >
+     <Router ref={this.routerRef}>
     <div className="App">
       <Navbar/>
       <Switch>
@@ -28,7 +52,8 @@ function App() {
       <Footer />
     </div>
     </Router>
-  );
+    </Context.Provider>
+  ); 
+  }
 }
 
-export default App;
