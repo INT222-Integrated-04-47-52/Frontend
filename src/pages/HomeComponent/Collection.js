54504e57288e408c91e8af6ab50e29ddc23ac1ac
Kpinnoes <khorapin.pingadpu@mail.kmutt.ac.th>
2021-12-07 01:19:54 +0700

    [Update] banner

--- src/pages/HomeComponent/Collection.js
+++ src/pages/HomeComponent/Collection.js
@@ -70,11 +70,11 @@ function Collection(){
                 <div class=" col-lg-7 offset-lg-3">
                     <div class="banner__item banner__item--last">
                        
-                        <div class="banner__item__text">
+                        <div class="banner__item__pic">
+                            <img src={banner2} alt=""/>
+                        </div> <div class="banner__item__text">
                             <h2>Silk Spring 2030</h2>
                            <li> <Link to="/Shop">Shop now</Link></li>
-                        </div> <div class="banner__item__pic">
-                            <img src={banner2} alt=""/>
                         </div>
                     </div>
                 </div>
