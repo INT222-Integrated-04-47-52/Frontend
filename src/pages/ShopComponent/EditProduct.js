24b1e8e3874b342c332887c496bc4b139a09abef
Prapaporn Sila <prapaporn.1412@mail.kmutt.ac.th>
2021-12-09 04:36:38 +0700

    [UI Edit]

--- src/pages/ShopComponent/EditProduct.js
+++ src/pages/ShopComponent/EditProduct.js
@@ -298,9 +298,9 @@ class EditProduct extends Component {
       >
         <div className="bg-white h-1/12" style={{ opacity: "1" }}>
           <div className="mx-5">
-            <div onClick={this.props.close}>
+            <button onClick={this.props.close}>
               <span className="material-icons text-5xl  ">close</span>
-            </div>
+            </button>
             <form onSubmit={this.save}>
               {/* <div style={{paddingLeft:"32px"}} className="text-left  font-black  text-5xl">
                             <h1 className="font-bold mt-4 ml-28">Add Product</h1>
@@ -310,8 +310,8 @@ class EditProduct extends Component {
                   style={{ paddingTop: "40px" }}
                   className="section-title"
                 >
-                  <h2 className="pt-24 pl-24 m-1.5">Edit Product</h2>
-                  <p className="pl-24 m-1.5">แก้ไขรายการสินค้า</p>
+                  <h2 className="pt-24 pl-2 m-1.5">Edit Product</h2>
+                  <p className="pl-2 m-1.5">แก้ไขรายการสินค้า</p>
                 </div>
               </div>
               <div className="flex place-items-center grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-0">
@@ -463,7 +463,7 @@ class EditProduct extends Component {
                       <br />
 
                       {/*className={{'border-red-600': this.state.colors.map(c => c.id).includes(color.id)}} */}
-                      <div className="grid grid-cols-1 md:grid-cols-6">
+                      <div className="grid lg:grid-cols-6 md:grid-cols-5 grid-cols-3">
                         {this.state.colors.map((c) => (
                           <div className="mx-2 my-1">
                             {" "}
@@ -504,7 +504,7 @@ class EditProduct extends Component {
                   <div className="field is-clearfix">
                     {/* <Link to="/Shop">  */}{" "}
                     <button
-                      className="primary-btn flex my-4 mx-auto justify-center"
+                      className="flex my-5 mx-auto justify-center transition delay-150 duration-300 ease-in-out mb-2 md:mb-0 bg-gray-900 px-5 py-2 tracking-wider border text-white hover:opacity-75"
                       type="submit"
                       onClick={this.save}
                     >
